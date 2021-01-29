cf2e7fef092746f609d9c5928aefe855b96a2d94
wyattcw <wyatt.wolly@gmail.com>
2021-01-28 21:32:50 -0700

    copied code over

---
+++ rand.py
@@ -0,0 +1,21 @@
+import random
+
+print('hi mom')
+
+min =-10
+max =400
+for i in range(11):
+    # points="200,min,250,190,160,2min"
+    print('points=','"',random.randint(min,max),random.randint(min,max/2),random.randint(min,max),random.randint(min,max/2),random.randint(min,max),random.randint(min,max/2),'"/>')
+
+
+# points=" 148, 144, 24, 64, 122, 44, "
+# points=" 126, 174, 22, 120, 125, 191, "
+# points=" 231, 49, 92, 136, 52, 149, "
+# points=" 191, 241, 163, 52, min7, 92, "
+# points=" 215, 62, 19, 54, 40, 19, "
+# points=" 228, 240, 34, 78, 250, 141, "
+# points=" 70, 71, min7, 161, 201, 72, "
+# points=" 128, 93, 180, 192, 89, 15, "
+# points=" 87, 140, 20, 55, 245, 190, "
+# points=" 116, 184, 238, 155, 200, 80, "
